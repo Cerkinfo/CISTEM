cefdb262f222dea0708b3bb0e7f50c269b65ce61
ju-none <julien.leclercqprof@gmail.com>
2026-01-21 05:03:11 +0100

    fix svg typo

--- interfaces/web/components/utils/coloredIcons.tsx
+++ interfaces/web/components/utils/coloredIcons.tsx
@@ -174,7 +174,7 @@ export const Graph = ({ size } : { size : string }) => (
 
 export const SmileyTooth = ({ size } : { size : string }) => (
     <svg width={size} viewBox="0 0 246 246" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g clip-path="url(#clip0_397_2943)">
+        <g clipPath="url(#clip0_397_2943)">
         <g filter="url(#filter0_iii_397_2943)">
         <path d="M0 115C0 51.4872 51.4873 0 115 0H127C192.722 0 246 53.2781 246 119V127C246 192.722 192.722 246 127 246H123C55.069 246 0 190.931 0 123V115Z" fill="url(#paint0_radial_397_2943)"/>
         </g>
@@ -190,17 +190,17 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <path d="M176.643 145.251L135.5 166.5L167.616 187.952C167.868 188.121 167.995 188.205 168.094 188.27C180.929 196.66 198.231 190.998 203.623 176.645C203.665 176.533 203.717 176.39 203.821 176.105C203.934 175.795 203.991 175.639 204.031 175.524C209.347 160.433 197.917 144.689 181.922 145.07C181.799 145.072 181.634 145.078 181.304 145.09L176.643 145.251Z" fill="#C4C4C4"/>
         <path d="M176.643 145.251L135.5 166.5L167.616 187.952C167.868 188.121 167.995 188.205 168.094 188.27C180.929 196.66 198.231 190.998 203.623 176.645C203.665 176.533 203.717 176.39 203.821 176.105C203.934 175.795 203.991 175.639 204.031 175.524C209.347 160.433 197.917 144.689 181.922 145.07C181.799 145.072 181.634 145.078 181.304 145.09L176.643 145.251Z" fill="url(#paint1_radial_397_2943)"/>
         </g>
-        <path d="M160.443 152.907C162.435 150.762 166.188 151.586 167.505 154.2C168.838 156.845 170.748 159.923 173.34 162.552C176.935 166.2 183.567 170.173 188.28 172.759C190.552 174.006 190.648 177.326 188.404 178.624C187.066 179.398 185.42 179.399 184.101 178.592C180.594 176.447 173.598 171.992 169.433 168.155C165.28 164.329 161.944 159.98 160.094 157.365C159.12 155.988 159.296 154.143 160.443 152.907Z" fill="#000000" fill-opacity="0.21"/>
+        <path d="M160.443 152.907C162.435 150.762 166.188 151.586 167.505 154.2C168.838 156.845 170.748 159.923 173.34 162.552C176.935 166.2 183.567 170.173 188.28 172.759C190.552 174.006 190.648 177.326 188.404 178.624C187.066 179.398 185.42 179.399 184.101 178.592C180.594 176.447 173.598 171.992 169.433 168.155C165.28 164.329 161.944 159.98 160.094 157.365C159.12 155.988 159.296 154.143 160.443 152.907Z" fill="#000000" fillOpacity="0.21"/>
         <g filter="url(#filter4_ii_397_2943)">
         <mask id="path-6-inside-1_397_2943" fill="white">
         <path d="M178.213 126.527C182.947 126.869 186.554 131.034 185.458 135.652C183.369 144.449 179.202 152.734 173.218 159.878C165.337 169.287 154.658 176.292 142.532 180.008C130.406 183.725 117.377 183.984 105.093 180.755C95.7517 178.299 87.16 173.91 79.9294 167.951C76.2321 164.904 76.6807 159.328 80.4149 156.326C83.8295 153.582 88.7497 154.033 92.2061 156.725C97.3062 160.697 103.261 163.64 109.702 165.333C118.733 167.707 128.313 167.516 137.229 164.784C146.144 162.051 153.996 156.901 159.791 149.982C163.724 145.287 166.59 139.92 168.251 134.214C169.559 129.72 173.545 126.19 178.213 126.527Z"/>
         </mask>
-        <path d="M178.213 126.527C182.947 126.869 186.554 131.034 185.458 135.652C183.369 144.449 179.202 152.734 173.218 159.878C165.337 169.287 154.658 176.292 142.532 180.008C130.406 183.725 117.377 183.984 105.093 180.755C95.7517 178.299 87.16 173.91 79.9294 167.951C76.2321 164.904 76.6807 159.328 80.4149 156.326C83.8295 153.582 88.7497 154.033 92.2061 156.725C97.3062 160.697 103.261 163.64 109.702 165.333C118.733 167.707 128.313 167.516 137.229 164.784C146.144 162.051 153.996 156.901 159.791 149.982C163.724 145.287 166.59 139.92 168.251 134.214C169.559 129.72 173.545 126.19 178.213 126.527Z" stroke="url(#paint2_linear_397_2943)" stroke-width="22" stroke-linejoin="round" mask="url(#path-6-inside-1_397_2943)"/>
+        <path d="M178.213 126.527C182.947 126.869 186.554 131.034 185.458 135.652C183.369 144.449 179.202 152.734 173.218 159.878C165.337 169.287 154.658 176.292 142.532 180.008C130.406 183.725 117.377 183.984 105.093 180.755C95.7517 178.299 87.16 173.91 79.9294 167.951C76.2321 164.904 76.6807 159.328 80.4149 156.326C83.8295 153.582 88.7497 154.033 92.2061 156.725C97.3062 160.697 103.261 163.64 109.702 165.333C118.733 167.707 128.313 167.516 137.229 164.784C146.144 162.051 153.996 156.901 159.791 149.982C163.724 145.287 166.59 139.92 168.251 134.214C169.559 129.72 173.545 126.19 178.213 126.527Z" stroke="url(#paint2_linear_397_2943)" strokeWidth="22" strokeLinejoin="round" mask="url(#path-6-inside-1_397_2943)"/>
         </g>
         </g>
         <defs>
-        <filter id="filter0_iii_397_2943" x="-11" y="-24" width="278" height="326" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter0_iii_397_2943" x="-11" y="-24" width="278" height="326" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feMorphology radius="8" operator="erode" in="SourceAlpha" result="effect1_innerShadow_397_2943"/>
@@ -222,8 +222,8 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <feColorMatrix type="matrix" values="0 0 0 0 1 0 0 0 0 0.766722 0 0 0 0 0.693056 0 0 0 0.68 0"/>
         <feBlend mode="normal" in2="effect2_innerShadow_397_2943" result="effect3_innerShadow_397_2943"/>
         </filter>
-        <filter id="filter1_i_397_2943" x="40.8352" y="81" width="67.5018" height="34.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter1_i_397_2943" x="40.8352" y="81" width="67.5018" height="34.5" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feMorphology radius="26.45" operator="dilate" in="SourceAlpha" result="effect1_innerShadow_397_2943"/>
@@ -233,8 +233,8 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <feColorMatrix type="matrix" values="0 0 0 0 0.462111 0 0 0 0 0.203767 0 0 0 0 0.504167 0 0 0 0.35 0"/>
         <feBlend mode="normal" in2="shape" result="effect1_innerShadow_397_2943"/>
         </filter>
-        <filter id="filter2_i_397_2943" x="137.835" y="81" width="67.5018" height="34.5" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter2_i_397_2943" x="137.835" y="81" width="67.5018" height="34.5" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feMorphology radius="26.45" operator="dilate" in="SourceAlpha" result="effect1_innerShadow_397_2943"/>
@@ -244,8 +244,8 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <feColorMatrix type="matrix" values="0 0 0 0 0.462111 0 0 0 0 0.203767 0 0 0 0 0.504167 0 0 0 0.35 0"/>
         <feBlend mode="normal" in2="shape" result="effect1_innerShadow_397_2943"/>
         </filter>
-        <filter id="filter3_iii_397_2943" x="132.234" y="134.175" width="73.1166" height="62.3635" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter3_iii_397_2943" x="132.234" y="134.175" width="73.1166" height="62.3635" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feOffset dy="4.35512"/>
@@ -266,8 +266,8 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <feColorMatrix type="matrix" values="0 0 0 0 0.828932 0 0 0 0 0.0596354 0 0 0 0 0.954167 0 0 0 0.6 0"/>
         <feBlend mode="normal" in2="effect2_innerShadow_397_2943" result="effect3_innerShadow_397_2943"/>
         </filter>
-        <filter id="filter4_ii_397_2943" x="77.3744" y="126.505" width="108.281" height="56.4954" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+        <filter id="filter4_ii_397_2943" x="77.3744" y="126.505" width="108.281" height="56.4954" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix"/>
         <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
         <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
         <feMorphology radius="26.45" operator="dilate" in="SourceAlpha" result="effect1_innerShadow_397_2943"/>
@@ -285,16 +285,16 @@ export const SmileyTooth = ({ size } : { size : string }) => (
         <feBlend mode="normal" in2="effect1_innerShadow_397_2943" result="effect2_innerShadow_397_2943"/>
         </filter>
         <radialGradient id="paint0_radial_397_2943" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(109 16) rotate(86.5167) scale(230.426)">
-        <stop offset="0.576389" stop-color="#FFBB0C"/>
-        <stop offset="1" stop-color="#FFC907"/>
+        <stop offset="0.576389" stopColor="#FFBB0C"/>
+        <stop offset="1" stopColor="#FFC907"/>
         </radialGradient>
         <radialGradient id="paint1_radial_397_2943" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(158.989 162.419) rotate(64.8545) scale(50.1444 41.4853)">
-        <stop stop-color="#FC4141"/>
-        <stop offset="1" stop-color="#FF0F0F"/>
+        <stop stopColor="#FC4141"/>
+        <stop offset="1" stopColor="#FF0F0F"/>
         </radialGradient>
         <linearGradient id="paint2_linear_397_2943" x1="87.398" y1="115.298" x2="127.504" y2="179.893" gradientUnits="userSpaceOnUse">
-        <stop stop-color="#402C41"/>
-        <stop offset="1" stop-color="#0C0B0E"/>
+        <stop stopColor="#402C41"/>
+        <stop offset="1" stopColor="#0C0B0E"/>
         </linearGradient>
         <clipPath id="clip0_397_2943">
         <rect width="246" height="246" fill="white"/>
