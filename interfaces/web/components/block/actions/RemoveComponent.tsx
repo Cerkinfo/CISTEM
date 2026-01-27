1364050064d8a4fb5d1ed3e34fe8f332f1890648
ju-none <julien.leclercqprof@gmail.com>
2026-01-27 02:11:46 +0100

    add remove component modal

---
+++ interfaces/web/components/block/actions/RemoveComponent.tsx
@@ -0,0 +1,75 @@
+import { ListHeaderBar } from "@front/components/bar/ListHeaderBar";
+import { Beer, Note, Sandwich, Soft } from "@front/components/utils/coloredIcons";
+import { useState } from "react";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import "@styles/components/modal.scss";
+import { ComponentDropdown } from "@front/components/form/dropdown/ComponentDropdown";
+import { useInformationsList } from "@pkg/hooks/list/getInformations";
+import { DrinkCard } from "../DrinkCard";
+
+export function RemoveComponent({ isOpen, close } : { isOpen: boolean, close: (b: boolean) => void}) {
+    const [listView, setListView] = useState('beers');
+    const [current, setCurrent] = useState<any>('');
+    const {list: data} = useInformationsList({tableName: listView as any})
+    const list = [
+        { key: 'beers', icon: <Beer size={'30'} />, name: 'Bières', 
+            view: (
+                <DrinkCard 
+                    name={`${current.name} ${current.volume}cl`}
+                    image={current.image}
+                    price={current.price}
+                />
+            )},
+        { key: 'softs', icon: <Soft size={'30'} />, name: 'Softs', 
+            view: (
+                <DrinkCard 
+                    name={`${current.name} ${current.volume}cl`}
+                    image={current.image}
+                    price={current.price}
+                />
+            )},
+        { key: 'foods', icon: <Sandwich size={'30'} />, name: 'Nourriture', 
+            view: (
+                <DrinkCard 
+                    name={current.name}
+                    image={current.image}
+                    price={current.price}
+                />
+            )},
+        { key: 'materials', icon: <Note size={'30'} />, name: 'Matériel', 
+            view: (
+                <DrinkCard 
+                    name={current.name}
+                    image={current.image}
+                    price={current.price}
+                />
+            )}
+    ];
+
+    function handleChangeView(view: string) {
+        setListView(view);
+        setCurrent('')
+    }
+    return(
+        <Modal isOpen={isOpen} size="xl" unmountOnClose={true}>
+            <ModalHeader>
+                <ListHeaderBar list={list} view={listView} onChange={handleChangeView} size={'1vw'}/>
+            </ModalHeader>
+                <ModalBody>
+                    <ComponentDropdown list={data} current={current} onChange={setCurrent}/>
+                    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                        {current && list?.find((l: any) => l.key === listView)?.view}
+                    </div>
+                </ModalBody>
+            <ModalFooter>
+                <p>ATTENTION ! Cette action est irréversible, il n'y a de sauvegarde nulle part dans la base de donnée !</p>
+                <Button outline color="danger" onClick={() => close(false)}>
+                    Cancel
+                </Button>
+                <Button outline color="warning" onClick={() => {}} disabled={!current}>
+                    Remove
+                </Button>
+            </ModalFooter>
+        </Modal>
+    )
+}
--- interfaces/web/components/block/actions/admin.tsx
+++ interfaces/web/components/block/actions/admin.tsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { SlideButton } from "@front/components/form/buttons/SlideButton";
 import { AddComponent } from "./AddComponent";
 import { EditComponent } from "./EditComponent";
+import { RemoveComponent } from "./RemoveComponent";
 
 export function Admin() {
     const [modal, setModal] = useState('');
@@ -14,8 +15,12 @@ export function Admin() {
             <li>
                 <SlideButton text={'Editer'} onClick={() => setModal('edit')} />
             </li>
+            <li>
+                <SlideButton text={'Supprimer'} onClick={() => setModal('remove')} />
+            </li>
             <AddComponent isOpen={modal === 'add'} close={() => setModal('')} />
             <EditComponent isOpen={modal === 'edit'} close={() => setModal('')} />
+            <RemoveComponent isOpen={modal === 'remove'} close={() => setModal('')} />
         </>
     )
 }
