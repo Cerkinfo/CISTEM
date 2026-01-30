37b85e88d2cc5fb11553276722d5fd8ee502b97a
ju-none <julien.leclercqprof@gmail.com>
2026-01-30 12:11:55 +0100

    add public menu page

--- apps/CISTEM/src/App.tsx
+++ apps/CISTEM/src/App.tsx
@@ -18,6 +18,7 @@ import { Benevoles } from '@front/pages/benevoles';
 import { Credits } from '@front/pages/credits';
 import { DrainIdActions } from '@front/pages/id/Drain';
 import { Drain } from '@front/pages/drain';
+import { Menu } from '@front/pages/menu';
 
 function App() {
   return (
@@ -27,21 +28,21 @@ function App() {
             <Route element={<PublicLayout />}>
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
-              <Route path="/fosdem-bar" element={<div>fosdem bar</div>} />
+              <Route path="/menu" element={<Menu />} />
             </Route>
 
             <Route path="/" element={<AuthGuard><Layout /></AuthGuard>}>
               <Route index element={<HowToFOSDEM />} />
               <Route path="profile/:pseudo" element={<Profile />} />
-              <Route path="schedule" element={<></>} />
+              {/* <Route path="schedule" element={<></>} /> */}
               <Route path="list" element={<List />} />
               <Route path="drain" element={<Drain />} />
               <Route path="drain/:id" element={<DrainIdActions />} />
               <Route path="inventory" element={<Inventory />} />
               <Route path='benevoles' element={<Benevoles />} />
               <Route path='credits' element={<Credits />} />
-              <Route path="*" element={<div>404 Not Found</div>} />
             </Route>
+            <Route path="*" element={<div>404 Not Found</div>} />
           </Routes>
         </BrowserRouter>
     </SessionProvider>
